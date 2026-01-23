d50f19c63737f3f1c06912413105be3243acf18f
shlok <shloknaik.mail@gmail.com>
2026-01-23 19:30:18 +0000

    Flashcard Functionality

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const button = document.getElementById('flashcard');
+const flashcard = document.getElementById("flashcard");
+if (flashcard) {
+  flashcard.addEventListener("click", () => {
+    flashcard.classList.toggle("flipped");
+  });
+}
 
-button.addEventListener('click', () => {
-    alert('You clicked the button!');
-});
+const addCardButton = document.getElementById("addCard");
+if (addCardButton) {
+  addCardButton.addEventListener("click", () => {
+    window.location.href = "add-card.html";
+  });
+}
+
+const closeButton = document.getElementById("closeButton");
+if (closeButton) {
+  closeButton.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
